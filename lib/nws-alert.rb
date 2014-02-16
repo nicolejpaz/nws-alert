e169a8c1ebf41c46ae43871d716e54a4fc47d73c
Nicole Paz <nicole@nic.gateway.2wire.net>
2014-02-15 21:28:59 -0800

    Created ability to search for warnings by zip code.
    It uses Nokogiri to get the zone code (e.g., NY003) for the specific zip code, then searches for the weather alerts using that code.
    
    Also included an error message in case the gem returns no active alerts.

--- lib/nws-alert.rb
+++ lib/nws-alert.rb
@@ -1,4 +1,6 @@
 require 'httparty'
+require 'nokogiri'
+require 'open-uri'
 require 'alert'
 
 module Nws
@@ -13,16 +15,59 @@ module Nws
     private
 
     def fetch(location)
-      xml = HTTParty.get("http://alerts.weather.gov/cap/#{location}.php?x=0", format: :xml)
+      unless /\d{5}/.match(location).nil?
+        xml = parse_weather_alerts_by_zone(location)
+      else
+        xml = HTTParty.get("http://alerts.weather.gov/cap/#{location}.php?x=0", format: :xml)
+      end
       parse_entries(xml)
+    end
+
+    def parse_weather_alerts_by_zone(location)
+      lat_long = get_lat_long(location)
+      lat = lat_long.gsub(/,.{7,}/, '')
+      long = lat_long.gsub(/.{7,},/, '')
+
+      zone = scrape_for_zone(lat, long)
+      
+      HTTParty.get("http://alerts.weather.gov/cap/wwaatmget.php?x=#{zone}&y=1", format: :xml)
+    end
+
+    def scrape_for_zone(lat, long)
+      doc = Nokogiri::HTML(open("http://forecast.weather.gov/MapClick.php?lat=#{lat}&lon=#{long}#.UwAsUmRdXue"))
+      div_containing_zone = doc.css('.current-conditions-extra')
+      anchor_containing_zone = div_containing_zone.css('a')
+      anchor_containing_zone.first.first[1].scan(/[A-Z]{3}\d{3}/).first
+    end
+
+    def get_lat_long(location)
+      date = Time.now.to_s.gsub(/\s.{1,}/, '')
+      HTTParty.get("http://graphical.weather.gov/xml/SOAP_server/ndfdXMLclient.php?whichClient=LatLonListZipCode&lat=&lon=&listLatLon=&lat1=&lon1=&lat2=&lon2=&resolutionSub=&listLat1=&listLon1=&listLat2=&listLon2=&resolutionList=&endPoint1Lat=&endPoint1Lon=&endPoint2Lat=&endPoint2Lon=&listEndPoint1Lat=&listEndPoint1Lon=&listEndPoint2Lat=&listEndPoint2Lon=&zipCodeList=&listZipCodeList=#{location}&centerPointLat=&centerPointLon=&distanceLat=&distanceLon=&resolutionSquare=&listCenterPointLat=&listCenterPointLon=&listDistanceLat=&listDistanceLon=&listResolutionSquare=&citiesLevel=&listCitiesLevel=&sector=&gmlListLatLon=&featureType=&requestedTime=&startTime=&endTime=&compType=&propertyName=&product=time-series&begin=#{date}T00%3A00%3A00&end=#{date}T00%3A00%3A00&Unit=e&wwa=wwa&Submit=Submit", format: :xml)['dwml']['latLonList']
     end
 
     def parse_entries(xml)
       entries = xml['feed']['entry']
-      entries.each do |entry|
-        item = HTTParty.get(entry['id'], format: :xml)['alert']
-        alert = Alert.new(item)
+      unless entries['title'] == "There are no active watches, warnings or advisories"
+        get_alerts_at_location(entries)
+      else
+        no_alerts_available
+      end
+    end
+
+    def get_alerts_at_location(entries)
+      # entries.each do |entry|
+        # item = HTTParty.get(entry[1], format: :xml)['alert']
+        item = HTTParty.get(entries['id'], format: :xml)['alert']
+        alert = Alert.new(item) unless item.nil?
         @alerts << alert
+      # end
+    end
+
+    def no_alerts_available
+      begin
+        raise StandardError, "There are no active watches or warnings in the area specified"
+      rescue Exception => e
+        puts e.message
       end
     end
   end
--- nws-alert.gemspec
+++ nws-alert.gemspec
@@ -6,7 +6,7 @@ Gem::Specification.new do |s|
   s.description = "Obtain weather alerts from NOAA. Look up information by zip code or state."
   s.authors     = ["Nicole Paz"]
   s.email       = 'me@nicolejpaz.com'
-  s.files       = ['lib/nws-alert.rb']
+  s.files       = ['lib/nws-alert.rb', 'lib/alert.rb']
   s.homepage    = 'https://github.com/nicolejpaz/nws-alert'
   s.license     = 'MIT'
 
